21d51e6844b32fe8c13326f892fc7c510d55aa63
Julie Souvandy <julie.souvandy.pro@gmail.com>
2023-05-31 08:14:06 +0200

    ajout de commentaires dans le html/js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,14 +1,27 @@
+// Création de la fonction editNav
 function editNav() {
+  //Création d'une variable x 
+  //qui correspond à l'id myTopnav soit la barre haute de l'entête
   var x = document.getElementById("myTopnav");
+
+  //Si la propriété className de cet élément 
+  //correspond en valeur et en nature à topnav
   if (x.className === "topnav") {
+  
+  //alors ajouter responsive au nom de la classe
     x.className += " responsive";
+
+  //sinon, 
   } else {
     x.className = "topnav";
   }
 }
 
 // DOM Elements
+// Création d'une constante modalbg
+//correspond à la div qui contient le formulaire d'inscription
 const modalbg = document.querySelector(".bground");
+
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
