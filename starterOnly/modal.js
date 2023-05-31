38af162625d57df490a827e1cd855d7e1439cb08
Julie Souvandy <julie.souvandy.pro@gmail.com>
2023-05-31 13:02:35 +0200

    résolution de l'issue #1 : fermer la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -18,19 +18,45 @@ function editNav() {
 }
 
 // DOM Elements
+
 // Création d'une constante modalbg
-//correspond à la div qui contient le formulaire d'inscription
+// correspond à bground
+// la div qui contient le formulaire d'inscription
 const modalbg = document.querySelector(".bground");
 
-const modalBtn = document.querySelectorAll(".modal-btn");
+//Création d'une constante formData
+// correspond à chaque champs de formulaire
 const formData = document.querySelectorAll(".formData");
 
+//Création d'une constante modalBtn
+//correspond au bouton je m'inscris
+const modalBtn = document.querySelectorAll(".modal-btn");
+
+//Création d'une constante closeButton
+//correspond à la croix dessinée dans le css
+const closeModalBtn = document.querySelectorAll(".close");
+
+//Ajout de de la méthode forEach sur le bouton je m'inscris
+//Pour chaque click sur le btn, on lance la fonction launchModal
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+//Fonction launchModal
+//display du div contenant le form à block
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+//Ajout de la méthode forEach sur l'élément close
+//Pour chaque click sur le close, on lance la fonction closeModal
+// close modal event
+closeModalBtn.forEach((closeBtn) => closeBtn.addEventListener("click", closeModal));
 
+//Fonction closeModal
+//display du div contenant le form à 
+function closeModal(){
+  modalbg.style.display = "absolute";
+  modalbg.style.width= 0;
+  modalbg.style.width= 0;
+}
